e0e9d424d8bcf018d32b7b87e1d69a53eaf81d40
Soumya Ranjan Nanda <srnranjan2005@gmail.com>
2025-08-27 21:46:25 +0530

    removed some bugs

--- Backend/Main.py
+++ Backend/Main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://saha-ai.vercel.app"
+    "https://saha-ai.vercel.app",
+    "https://saha-ai.vercel.app/"
 ]
 app.add_middleware(
     CORSMiddleware,
